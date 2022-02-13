e022885c70965332095315a6558665d0ee8265b5
huzzh3 <huzzh3@outlook.com>
2022-02-13 16:08:47 +0800

    finish home development

--- main.js
+++ main.js
@@ -3,6 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+// 挂载
+uni.$http = $http
+// 设置请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title='数据请求失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
